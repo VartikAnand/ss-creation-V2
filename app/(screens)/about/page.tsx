e206bab876711a32745f14026b6571dff3841e0b
Vartik Anand <vartikkanand@gmail.com>
2023-07-05 16:53:16 +0530

    added about and contact

--- app/(screens)/about/page.tsx
+++ app/(screens)/about/page.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { MdOutlineGirl } from 'react-icons/md';
+import { RiContactsFill } from 'react-icons/ri';
+import Link from 'next/link';
+import shruti from "./../../../public/img/shruti.png";
+import Image from 'next/image';
 
 const About = () => {
   return (
-    <div className='h-screen'>About</div>
-  )
+    <section className="text-description-color body-font">
+      <div className="container mx-auto flex px-5 py-5 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Hi</h1>
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">I'm <span className='text-primary font-bold'>Shruti</span></h1>
+          <h3 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">3D artist and Animator</h3>
+          <h2>I bring imagination to life through captivating 3D art and animations, specializing in modeling, texturing, rigging, and animation using industry-standard tools.</h2>
+          <div className="flex lg:flex-row md:flex-col pt-8 mt-10 gap-10">
+            <Link href="">
+              <button className="bg-primary inline-flex py-3 px-5 rounded-lg items-center hover:bg-pink-400 focus:outline-none">
+                <MdOutlineGirl className="text-lg text-white mb-1" />
+                <span className="ml-4 flex items-center flex-col leading-none justify-center align-middle">
+                  <span className="text-xl text-white mb-1">Hire Me</span>
+                </span>
+              </button>
+            </Link>
+            <Link href="">
+              <button className="bg-primary inline-flex py-3 px-5 rounded-lg items-center hover:bg-pink-400 focus:outline-none">
+                <RiContactsFill className="text-lg text-white mb-1" />
+                <span className="ml-4 flex items-center flex-col leading-none justify-center align-middle">
+                  <span className="text-xl text-white mb-1">Contact Me</span>
+                </span>
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="relative lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mx-auto">
+          <Image
+            className="w-full h-full"
+            src={shruti}
+            alt="img"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default About
+export default About;
--- app/(screens)/contact/page.tsx
+++ app/(screens)/contact/page.tsx
@@ -1,9 +1,80 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react';
 
-const Contact = () => {
+const Contact: React.FC = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const mailtoLink = `mailto:sscreation@proton.me?subject=Contact%20Form%20Submission&body=Name:%20${name}%0D%0AEmail:%20${email}%0D%0AMessage:%20${message}`;
+    window.location.href = mailtoLink;
+  };
+
   return (
-    <div className='h-screen'>Contact</div>
-  )
+    <div className="text-description-color body-font relative">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-12">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            <span className='text-primary font-bold'>Contact Me</span>
+          </h1>
+        </div>
+        <div className="lg:w-1/2 md:w-2/3 mx-auto">
+          <div className="flex flex-wrap -m-2">
+            <div className="p-2 w-1/2">
+              <div className="relative">
+                <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
+                <input type="text" id="name" name="name" value={name} onChange={e => setName(e.target.value)} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              </div>
+            </div>
+            <div className="p-2 w-1/2">
+              <div className="relative">
+                <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
+                <input type="email" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              </div>
+            </div>
+            <div className="p-2 w-full">
+              <div className="relative">
+                <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
+                <textarea id="message" name="message" value={message} onChange={e => setMessage(e.target.value)} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" />
+              </div>
+            </div>
+            <div className="p-2 w-full">
+              <button className="flex mx-auto text-white bg-primary border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" onClick={handleSubmit}>Button</button>
+            </div>
+            <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+              <div className="text-indigo-500">sscreation@proton.me</div>
+              <span className="leading-normal my-5">Based in<br />India</span>
+              <span className="inline-flex">
+                <div className="text-gray-500">
+                  <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
+                  </svg>
+                </div>
+                <div className="ml-4 text-gray-500">
+                  <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
+                  </svg>
+                </div>
+                <div className="ml-4 text-gray-500">
+                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+                  </svg>
+                </div>
+                <div className="ml-4 text-gray-500">
+                  <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+                  </svg>
+                </div>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
